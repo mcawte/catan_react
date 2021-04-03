fc8cc8200412c4acd3db28561178c0ef85af484e
mcawte <mcawte@gmail.com>
2021-04-03 17:56:53 +1300

    Linter forcing gameState dependency in useEffect. Not ideal, tempory commit to get deployment working

--- src/components/logic/BoardLogic.tsx
+++ src/components/logic/BoardLogic.tsx
@@ -94,7 +94,7 @@ export default function BoardLogic({
       gameState
     );
     return () => {};
-  }, [boardUpdate]);
+  }, [boardUpdate, gameState]);
 
   const robberDebtMet = (): boolean => {
     let resourceSelected =
