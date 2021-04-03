961bf82118cccfd0f6f0d1a8b1df69c8f6191598
mcawte <mcawte@gmail.com>
2021-04-03 19:38:04 +1300

    with credentials

--- src/service/socket.ts
+++ src/service/socket.ts
@@ -6,7 +6,12 @@ const localToken = localStorage.getItem('token')
 
 console.log("The locally stored token is: ", localToken)
 
-export const socket = io(socketUrl, {auth: {token: localToken}})
+export const socket = io(socketUrl, {
+    auth: {token: localToken},
+    withCredentials: true
+})
+
+
 
 socket.on("newToken", (token: any) => {
     console.log("saving new token to local stroage")
