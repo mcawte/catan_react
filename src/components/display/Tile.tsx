38a9cd9f3ff8b979f47f057efeb62b3af1168555
mcawte <mcawte@gmail.com>
2021-04-06 19:58:14 +1200

    test vw font

--- src/components/display/Tile.tsx
+++ src/components/display/Tile.tsx
@@ -54,7 +54,7 @@ export default function Tile(props: React.PropsWithChildren<TileProps>) {
   const topShift =
     (props.tileSize / 2) * Math.sqrt(3) * props.row * props.gridSpace;
 
-  const textShadowSize = 1.2 //tileWidth / 110;
+  const textShadowSize = tileWidth / 110;
 
   return (
     <>
@@ -87,7 +87,8 @@ export default function Tile(props: React.PropsWithChildren<TileProps>) {
               <div
                 className="tileNumber"
                 style={{
-                  fontSize: "1.2rem",
+                  //fontSize: `${tileWidth / 110}em`,
+                  fontSize: "15vw",
                   textShadow: `-${textShadowSize}px -${textShadowSize}px 0 #000, ${textShadowSize}px -${textShadowSize}px 0 #000, -${textShadowSize}px ${textShadowSize}px 0 #000, ${
                     1.25 * textShadowSize
                   }px ${1.25 * textShadowSize}px 0 #000`,
