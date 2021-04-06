4f64a521325f91c6b5979c6a4928cdd2f85bb572
mcawte <mcawte@gmail.com>
2021-04-06 20:46:45 +1200

    stroke

--- src/components/display/Tile.tsx
+++ src/components/display/Tile.tsx
@@ -91,7 +91,8 @@ export default function Tile(props: React.PropsWithChildren<TileProps>) {
                   // textShadow: `-${textFontSize}px -${textFontSize}px 0 #000, ${textFontSize}px -${textFontSize}px 0 #000, -${textFontSize}px ${textFontSize}px 0 #000, ${
                   //   0.05 * textFontSize
                   // }px ${0.05 * textFontSize}px 0 #000`,
-                  textShadow: `${0.1 * textFontSize}px ${0.1 * textFontSize}px #000`,
+                  //textShadow: `${0.1 * textFontSize}px ${0.1 * textFontSize}px #000`,
+                  WebkitTextStroke: "10px black"
                 }}
               >
                 {props.robber ? (
