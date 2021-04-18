bbbd5596f0d2bf0307684b00e394979e7da0b34b
mcawte <mcawte@gmail.com>
2021-04-18 18:43:48 +1200

    transform

--- src/components/containers/Tiles.tsx
+++ src/components/containers/Tiles.tsx
@@ -87,10 +87,9 @@ export default function Tiles({
         top: `${tileSize / 10}px`,
         left: `${tileSize / 10}px`,
         width: "53vw",
-       // height: "85vh",
+        height: "85vh",
         overflow: "auto",
         //borderStyle: "dotted"
-        //transform: "rotate(90deg)"
       }}
       ref={componentRef}
     >
